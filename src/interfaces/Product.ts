b5db69152ec196aa22888d565b31d320b2c507b1
Weronika Graczyk <wercia01@onet.pl>
2023-08-11 20:28:01 +0200

    update product interface

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,6 +1,17 @@
-export interface Product {
+export interface IProduct {
     id?: number | null,
-    name?: string | null,
-    price?: number | null,
+    name?: string,
+    price?: number,
+}
+
+export class Product implements IProduct{
+    public id: null;
+    public name: string;
+    public price: number;
     
+    constructor(){
+        this.id =null;
+        this.name = "";
+        this.price = 0;
+    }
 }
